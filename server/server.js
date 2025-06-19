1115142105af72bd62dcc6faf255d7874294e196
BM-ACADEMY <teamkamarweb@gmail.com>
2025-06-19 10:56:12 +0530

    push

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "*",
+  origin: "https://bmtechx-projectgym.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
